d68241429e89d89931a61aced266041ce417749a
lahcenbcf <h.bencharef@esi-sba.dz>
2023-12-14 14:24:24 +0000

    mergeHomePageToFrontenTeam

--- client/src/components/HomeNavbar.jsx
+++ client/src/components/HomeNavbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import docLibLogo from "../assets/Logo.svg"
+import docLibLogo from "../assets/docLibLogo.svg"
 import { Link } from 'react-router-dom'
 function HomeNavbar() {
   return (
     <header className='bg-white px-3'>
     <navbar className="container mx-auto flex justify-between items-center">
       {/* logo */}
-      <div className='w-[9rem]'>
+      <div className='w-[12rem] mt-3'>
       <img src={docLibLogo} className='w-[100%]' alt='docLibLogo' />
       </div>
 
       <div className=' flex gap-8 lg:gap-20 items-center '>
+      {/* a propos */}
+      <Link className='font-semibold'>A PROPOS</Link>
           {/* About */}
       <Link className='font-semibold'>FAQ</Link>
       {/* login/signUp */}
--- client/src/pages/HomeScreen.jsx
+++ client/src/pages/HomeScreen.jsx
@@ -1,15 +1,15 @@
 import homeImage from "../assets/home-image.svg"
 function HomeScreen() {
   return (
-    <div className="container mx-auto w-full px-3 pt-6 max-h-screen flex flex-wrap items-center">
+    <div className="container mx-auto w-full px-3 pt-20 lg:pt-6 max-h-screen flex flex-wrap items-center">
     {/* left side */}
-    <div className="flex-fluid ml-4">
+    <div className="flex-fluid ml-4 mb-10">
         <h3 className="font-bold">Trouvez Vos Articles en Un Clin d'OEil!</h3>
         <div className="my-8 mb-4">
         <h1 className="font-extrabold">DocLib</h1>
-        <p className="text-black w-10/12 my-4">la bibliothèque numérique qui simplifie la recherche d'articles. Explorez une collection diversifiée d'articles scientifiques, d'actualités, de revues spécialisées et bien plus encore, le tout à portée de clic.</p>
+        <p className="text-black w-3/5 my-6">la bibliothèque numérique qui simplifie la recherche d'articles. Explorez une collection diversifiée d'articles scientifiques, d'actualités, de revues spécialisées et bien plus encore, le tout à portée de clic.</p>
         </div>
-        <button className="hidden md:block Btn1 px-6 py-5 text-xl shadow-lg text-white w-3/4 bg-primaryColor">Découvrir la collection</button>
+        <button className="hidden md:block Btn1 px-3 py-5 text-2xl shadow-lg text-white w-3/5 bg-primaryColor my-8">Découvrir la collection</button>
     </div>
         {/* right side */}
         <div className="flex-fluid flex flex-col items-center">
