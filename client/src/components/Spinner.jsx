e194b2e5f2c33947acd24d237ac9d814f8c299ff
lahcenbcf <h.bencharef@esi-sba.dz>
2023-12-28 18:40:28 +0000

    fix main

--- client/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-function Spinner() {
-  return (
-    <div className="absolute inset-0 h-full flex justify-center items-center bgGradient z-10">
-        <span className="loading loading-spinner loading-md"></span>
-    </div>
-  )
-}
-
-export default Spinner
