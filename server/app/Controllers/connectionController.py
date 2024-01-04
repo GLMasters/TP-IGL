e99d6f81d0edcf0c27a571b762880d5cb82b6902
F1OOw <ls_djerrai@esi.dz>
2024-01-04 12:10:02 +0100

    implemented new errors logic and some docker

--- server/app/Controllers/connectionController.py
+++ server/app/Controllers/connectionController.py
@@ -1,22 +1,14 @@
 from Controllers.baseController import *
-from flask import jsonify, request
 import jwt
-from datetime import datetime, timedelta
+from datetime import datetime
 from validate_email_address import validate_email
 
 from Utils import *
 from Models.models import *
 
 from config import *
-import os
-
 from random import randint
 
-SECRET_KEY = os.getenv('SECRET_KEY')
-
-
-
-#check if the email exist
 def is_valid_email(email):
     
     is_valid_domain = validate_email(email, verify=True)
@@ -28,13 +20,14 @@ def registerFunction(request):
         in_email = request.json['email']
         in_passwd = request.json['password']
         if (not in_email or not in_passwd):
-            return empty_creds
+            return error(EMPTY_FIELD)
+        
         if (not is_valid_email(in_email)):
-           return invalid_email
+           return response(INVALID_INPUT)
 
         user = db.session.query(User).filter_by(email=in_email).first()
         if (not user == None):
-            return user_exists
+            return error(RESSOURCE_EXISTS)
         
         code = randint(10000,99999)
         
@@ -58,14 +51,10 @@ def registerFunction(request):
         send_verif_code(in_email,code)
 
             
-        return sendResponse(
-            data=user.toJSON(),
-            message='Code sent to email'
-        )
-    except Exception as e:
-        return sendErrorMessage(
-            message=str(e)
-        )
+        return response(OK, data=user.toJSON())
+    
+    except :
+        return error(INTERNAL_ERROR)
 
 def confirmEmail(request):
     try:
@@ -73,16 +62,16 @@ def confirmEmail(request):
         code = request.json['code']
 
         if (not id or not code):
-            return empty_code_id
+            return error(EMPTY_FIELD)
         
 
         tmp_user = db.session.query(TempUser).filter_by(id=id).one()
 
         if (not tmp_user):
-            return wrong_id
+            return error(RESSOURCE_DOESNT_EXIST)
         
         if (tmp_user.code != code):
-            return invalid_code
+            return error(INVALID_INPUT)
         
         new_user = User(
             email=tmp_user.email,
@@ -97,93 +86,88 @@ def confirmEmail(request):
         db.session.delete(tmp_user)
         db.session.commit()
 
-        return sendResponse(
-            data=new_user.toJSON(),
-            message='Account created successfull, you can login now'
-        )
+        return response(OK,data=new_user.toJSON())
     
-    except Exception as e:
-        return sendErrorMessage(
-            message=str(e)
-        )
+    except :
+        return error(INTERNAL_ERROR)
 
 def loginFunction (request):
     try: 
         in_email = request.json['email']
         in_passwd = request.json['password']
         if (not in_email or not in_passwd):
-            return empty_creds
+            return error(EMPTY_FIELD)
+        
         user =  db.session.query(User).filter_by(email=in_email).first()
         if (user and user.check_password(in_passwd)):
             token = generate_normal_token(user)
             
-            return sendResponse(
+            return response(
+                OK ,
                 data={
                     "token": token,
                     "role_id": user.role_id
-                },
-                message='Logged in successfully'
+                } 
             )
+
         else :
-            return failed_auth
-    except Exception as e:
-        return sendErrorMessage(
-            message=str(e)
-        )
+            return error(INVALID_CREDS)
+    
+    except :
+        return error(INTERNAL_ERROR)
     
 def logoutFunction(request):
-    token = request.headers.get('Authorization').split('Bearer ')[1]
+    try:
+        token = request.headers.get('Authorization').split('Bearer ')[1]
 
-    blacklistToken(token)
+        blacklistToken(token)
 
-    return {
-        "result": True,
-        "message": "Logged Out successfully" 
-    }
+        return response(OK)
+    
+    except :
+        return error(INTERNAL_ERROR)
 
 def resetToken(request):
     try:
         in_email= request.json['email']
 
         if (not validate_email(in_email)):
-            return invalid_email
+            return error(EMPTY_FIELD)
         
         user = db.session.query(User).filter_by(email=in_email).first()
 
         if (not user):
-            return user_inexistant
+            return error(RESSOURCE_DOESNT_EXIST)
         
         token = generate_reset_token(user)
         send_reset_token(user.email,token)
 
-        return sendResponse(data=None,message="Token sent to email")
-    except Exception as e:
-        return sendErrorMessage(
-            message=str(e)
-        )
+        return response(OK)
+    
+    except :
+        return error(INTERNAL_ERROR)
 
 def verifyResetToken(token):
 
     if isBlacklisted(token):
-        return jsonify({'Message': 'Token has expired (blacklist)'}), 403
+        return error(EXPIRED_TOKEN)
 
     try:
         data = decode_token(token)
     except jwt.ExpiredSignatureError:
-        return jsonify({'Message': 'Token has expired'}), 403
+        return error(EXPIRED_TOKEN)
     except jwt.InvalidTokenError:
-        return jsonify({'Message': 'Invalid token'}), 403
+        return error(INVALID_TOKEN)
     
     if (not data['reset']):
-        return jsonify({'Message': 'Invalid Reset token'}), 403
+        return error(INVALID_TOKEN)
     
-    return sendResponse(
-        
+    return response(
+        OK,
         data={
             "token": token,
             "role_id": data['user']['role_id']
-        },
-        message='Token Verified Successfully'
+        }
     )
     
 def reset_password(request):
@@ -193,19 +177,18 @@ def reset_password(request):
         password = request.json['password']
         token = extract_token(request)
         user_id = decode_token(token)['user']['id']
-
         
         if (not password or not user_id):
-            return empty_password_id
+            return error(EMPTY_FIELD)
 
         if (not token['reset']):
-            return jsonify({'Message': 'Invalid token'}), 403
+            return error(INVALID_TOKEN)
         
 
         user = db.session.query(User).filter_by(id=user_id).first()
 
         if (not user):
-            return user_inexistant
+            return error(RESSOURCE_DOESNT_EXIST)
         
         user.set_hashed_password(password)
 
@@ -214,17 +197,13 @@ def reset_password(request):
 
         token = generate_normal_token(user)
         
-        return sendResponse(
+        return response(
+            OK,
             data={
                 "token": token,
                 "role_id": user.role_id
-            },
-            message='Password Changed successfully'
+            }
         )
 
-    except Exception as e:
-        return sendErrorMessage(
-            message=str(e)
-        )
-
-
+    except :
+        return error(INTERNAL_ERROR)
